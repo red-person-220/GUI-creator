ea44250f56c2e705e76529193b7823930a30c03e
My name it's not important <lucasvarelacorrea@gmail.com>
2024-06-22 03:45:48 +0200

    remove package command

--- src/test_data_given_by.py
+++ src/test_data_given_by.py
@@ -8,23 +8,14 @@ def command(arguments):
     if not len(arguments) > 0:
         return 'Command Error: None argument given'
 
-    if not arguments[0] in ['project', 'package']:
+    if not arguments[0] in ['project', 'execute']:
         return 'Command Error: Argument given unknown'
 
     if not len(arguments) > 1:
         return 'Command Error: None name given'
 
-    if arguments[0] == 'package':
-        if not len(arguments) == 3:
-            return 'Command Error: There are not enough arguments. Example: GUIC packpage "name_user" "name_project" "name_branch"'
-
-        #url = f"https://github.com/{arguments[1]}/{arguments[2]}/archive/{arguments[3]}.zip"
-    
-        if not os.path.isdir('package'):
-            return 'Command Error: package folder do not find'
-        
-        if not os.path.isfile('package.json'):
-            return 'Command Error: package.json do not find'
+    if arguments[0] == 'execute':
+        #TODO
     else:
         if os.path.isdir(arguments[1]):
             return 'Command Error: The folder already exists'
