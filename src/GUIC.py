d60aecbb062faf9141b53ed6031df23461ed88f4
Lucas Varela Correa <lucasvarelacorrea@gmail.com>
2024-06-23 22:17:02 +0200

    code restructuring

--- src/GUIC.py
+++ src/GUIC.py
@@ -1,23 +1,28 @@
-"""
-    This file is the main file
-"""
-
-import sys, test_data_given_by, commands
+import sys, os, commands
 
 if __name__ == '__main__':
     arguments = sys.argv[1:]
 
-    error = test_data_given_by.command(arguments)
+    try:
+        if arguments[0] == 'project':
+            os.mkdir(arguments[1])
+            
+            with open(f'{arguments[1]}/main.guic', 'w'):
+                pass
+            
+            os.mkdir(f'{arguments[1]}/package')
 
-    if not error == None:
-        print(error)
-        exit()
+        elif arguments[0] == 'execute':
+            with open(arguments[1], 'r') as file:
+                for line in file:
+                    line = line.split()
 
-    if arguments[0] == 'project':
-        commands.project(name=arguments[1])
-    elif arguments[0] == 'packge':
-        commands.package(name_user=arguments[1], name_project=arguments[2])
+                    str_command = f'commands.{line[0]}({line[1:]})'
 
-    elif arguments[0] == 'execute':
-        test_data_given_by.file(name_file=arguments[1])
-        commands.execute(arguments[1])
+                    exec(str_command)
+
+        else:
+            raise NameError('The command does not exists')
+
+    except Exception as e:
+        print(f'GUIC errror: {e}')
